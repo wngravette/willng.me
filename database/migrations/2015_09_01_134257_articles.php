ebbb13f92a62d3a1a70ed4d0cb654bad268abdc7
William Naughton-Gravette <william@n-r.co>
2015-09-02 00:18:01 +1000

    DB framework beginnings

--- database/migrations/2015_09_01_134257_articles.php
+++ database/migrations/2015_09_01_134257_articles.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Articles extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->string('article_headline');
+            $table->string('article_catch');
+            $table->text('article_body');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('articles');
     }
 }
