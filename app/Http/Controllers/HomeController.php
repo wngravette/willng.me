1b655a5493796bc73933a3701cde2c5748421896
William Naughton-Gravette <william@n-r.co>
2015-09-01 16:32:42 +1000

    added catches

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
 {
     public function render()
     {
-        $name_catches = ['Melbournian', 'Lord of Dance', 'Septuple Threat', 'Swimwear Model'];
+        $name_catches = ['Melbournian', 'Lord of Dance', 'Septuple Threat', 'Swimwear Model', '60% Cheese', 'Purveyor of Cheap Wine'];
         $name_catch = $name_catches[rand(0, count($name_catches) - 1)];
 
         return view('home', ['name_catch' => $name_catch]);
