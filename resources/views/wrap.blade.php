113ae5b34137eca1f20c0e425df63a62594947d1
William Naughton-Gravette <william@n-r.co>
2015-09-01 16:31:34 +1000

    added pure.css locally

--- resources/views/wrap.blade.php
+++ resources/views/wrap.blade.php
@@ -3,15 +3,17 @@
     <head>
         <meta charset="utf-8">
         <title>William Naughton-Gravette</title>
-        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+        <link rel="stylesheet" href="{{asset('css/pure.css')}}">
         <!--[if lte IE 8]>
-            <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-old-ie-min.css">
+            <link rel="stylesheet" href="{{asset('css/grid-old-ie.css')}}">
         <![endif]-->
         <!--[if gt IE 8]><!-->
-            <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
+            <link rel="stylesheet" href="{{asset('css/grid.css')}}">
         <!--<![endif]-->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="{{asset('js/highcharts.js')}}"/></script>
         <script src="https://use.typekit.net/dtm4seu.js"></script>
         <script>try{Typekit.load({ async: true });}catch(e){}</script>
         @yield('additional_head')
