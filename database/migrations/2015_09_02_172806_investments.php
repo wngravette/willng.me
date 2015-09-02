18255a72527aaa0cb41ac6466fef5cfdae5763fe
William Naughton-Gravette <william@n-r.co>
2015-09-02 17:45:35 +1000

    Investment data DB

---
+++ database/migrations/2015_09_02_172806_investments.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Investments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('investments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('investment_hash');
+            $table->string('ticker');
+            $table->integer('number_owned');
+            $table->decimal('price_bought', 6, 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('investments');
+    }
+}
