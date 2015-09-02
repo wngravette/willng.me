5b5e5f7b1f2fd8abf1fda4825b1a1d6d23184aea
William Naughton-Gravette <william@n-r.co>
2015-09-02 21:23:10 +1000

    CIV migration

---
+++ database/migrations/2015_09_02_182557_CIVtotals.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CIVtotals extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('CIVtotals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('record_hash');
+            $table->decimal('amount', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('CIVtotals');
+    }
+}
