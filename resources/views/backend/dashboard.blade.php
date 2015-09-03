521ea9211e783288de161e73aa8905d5f09f11e4
William Naughton-Gravette <william@n-r.co>
2015-09-03 15:10:07 +1000

    articles index

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -2,8 +2,16 @@
 @section('content')
 <div class="columns">
     <div class="two-thirds column">
-        <h3>William Naughton-Gravette</h3>
-        <a class="btn" href="#" role="button">New Blog Post</a>
+        <h3>Actions</h3>
+        <a class="btn" href="new-post" role="button">New Blog Post</a>
+        <a class="btn" href="#" role="button">New Investment</a>
+        <h3>Blog Posts</h3>
+        @foreach ($blog_posts as $post)
+        <div class="blog_post">
+            <h2>{{$post->article_headline}}</h2>
+            <p><span class="state state-open">Online</span>Published {{$post->human_time}}</p>
+        </div>
+        @endforeach
     </div>
     <div class="one-third column">
         <script>
