cf2c93fa8cedaee48a8ccf27db1859ddc2c6e916
William Naughton-Gravette <william@n-r.co>
2015-09-05 21:41:29 +1000

    added 503 and 404 error pages

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,22 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Be right back.</title>
-
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Be right back.</div>
-            </div>
+@extends('wrap')
+@section('content')
+<div class="pure-g header">
+    <div class="pure-u-1">
+        <div class="l-box">
+            <h1>William Naughton-Gravette <span class="title_suppliment">503</h1>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+<div class="pure-g">
+    <div class="pure-u-20-24 pure-u-lg-16-24">
+        <div class="l-box">
+            <p>This is an error page.</p>
+            <p>It's a 503 error, which means one of three things has happened.</p>
+            <p>Firstly, I could have pulled the site offline myself. This accours approximately once every three days when I accidently upload graphic obscene images of myself to my blog and then deeply regret putting off developing a 'delete post' feature.</p>
+            <p>The second possibility is that Amazon Web Services, in charge of hosting the computer which this website lives inside of, has in some way neglected to realise that their data center is on fire, and, in turn, neglected to put it out.</p>
+            <p>Thirdly, and I put this last intentionally as it is highly ludicious and completely unreasonale, because it would mean that more than several people are on this site at once, and we know that's never going to happen. Who the hell do you think I am? In any case,</p>
+            <p>Sorry.</p>
+        </div>
+    </div>
+</div>
+@endsection
