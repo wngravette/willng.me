1f38264daf769dffcb236a5162c0686b78cd67d3
William Naughton-Gravette <william@n-r.co>
2015-09-05 21:52:52 +1000

    disabled registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,8 +29,8 @@ Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+//Route::get('auth/register', 'Auth\AuthController@getRegister');
+//Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 //Dev routes...
 Route::get('dev/asx', 'DevController@asx');
