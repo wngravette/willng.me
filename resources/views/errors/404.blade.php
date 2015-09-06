fc89d7cfe485ce5c5161048a535b98709e1f88b1
William Naughton-Gravette <william@n-r.co>
2015-09-06 10:56:02 +1000

    spell-check

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -12,9 +12,9 @@
         <div class="l-box">
             <p>This is an error page.</p>
             <p>It's a 404 error, which means whatever page you're looking for, it's not there.</p>
-            <p>Allow me to explain, as one of a few things may have occured. The first is my own negligence. This would be unsurprising to both me and you if you know me as I am useless in most senses.</p>
-            <p>The second is that you were manually trying to navigate this website as if you were in 2008 or are desparately in search of some hidden directory (which absolutely doesn't exist.)</p>
-            <p>The third is that I told you to come here in my own excitment for some feature or page that I have yet to actually deploy. In any case,</p>
+            <p>Allow me to explain, as one of a few things may have occurred. The first is my own negligence. This would be unsurprising to both me and you if you know me as I am useless in most senses.</p>
+            <p>The second is that you were manually trying to navigate this website as if you were in 2008 or are desperately in search of some hidden directory (which absolutely doesn't exist.)</p>
+            <p>The third is that I told you to come here in my own excitement for some feature or page that I have yet to actually deploy. In any case,</p>
             <p>Sorry.</p>
         </div>
     </div>
