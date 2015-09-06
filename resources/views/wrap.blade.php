507a9bc33c9c4ddb2b01d8e477d6f7eaa8b64061
William Naughton-Gravette <william@n-r.co>
2015-09-06 19:51:08 +1000

    added GA

--- resources/views/wrap.blade.php
+++ resources/views/wrap.blade.php
@@ -40,6 +40,16 @@
         <meta name="application-name" content="William Naughton-Gravette"/>
         <meta property=”og:title” content=”William Naughton-Gravette”/>
         <meta name=”description” content=”The personal website of full-stack developer and sugar daddy, William Naughton-Gravette.”>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-41377781-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
         @yield('additional_head')
     </head>
     <body>
