2a9b261d04b06434cffc17d8d459b6471f1ce243
William Naughton-Gravette <william@n-r.co>
2015-09-15 00:46:07 +1000

    expanded footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,4 @@
+<p>You can learn more about me and all of the v cool things I do here, and follow me on shit below:</p>
 <p>
     <a href="https://github.com/wngravette/willng.me"><i class="fa fa-github"></i></a>
     <a href="https://twitter.com/willpsng"><i class="fa fa-twitter"></i></a>
