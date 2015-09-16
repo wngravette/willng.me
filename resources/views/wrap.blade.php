7f0f841ccdf8f6a8d1af6355369e962e8c5143f2
William Naughton-Gravette <william@n-r.co>
2015-09-16 21:08:42 +1000

    added rainbow + theme into DOM

--- resources/views/wrap.blade.php
+++ resources/views/wrap.blade.php
@@ -13,8 +13,12 @@
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
+        <link rel="stylesheet" href="{{asset('css/rainbow-theme/github.css')}}"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="{{asset('js/highcharts.js')}}"/></script>
+        <script src="{{asset('js/rainbow.min.js')}}"/></script>
+        <script src="{{asset('js/rainbow-lang/generic.js')}}"/></script>
+        <script src="{{asset('js/rainbow-lang/php.js')}}"/></script>
         <script src="{{asset('js/chart-themes/gray.js')}}"/></script>
         <script src="https://use.typekit.net/dtm4seu.js"></script>
         <script>try{Typekit.load({ async: true });}catch(e){}</script>
