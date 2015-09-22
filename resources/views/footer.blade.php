401601d1ee31280fa6d6ddba9887fd4f0ed78426
William Naughton-Gravette <william@willng.me>
2015-09-22 20:46:52 +1000

    UI minors

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,7 @@
-<p>You can learn more about me and all of the v cool things I do here, and follow me on shit below:</p>
-<p>
-    <a href="https://github.com/wngravette/willng.me"><i class="fa fa-github"></i></a>
-    <a href="https://twitter.com/willpsng"><i class="fa fa-twitter"></i></a>
-</p>
+<div class="pure-u-1 footer">
+    <p>You can learn more about me and all of the v cool things I do <a href="about">here</a>, and follow me on shit below:</p>
+    <p>
+        <a class="icon" href="https://github.com/wngravette/willng.me"><i class="fa fa-github"></i></a>
+        <a class="icon" href="https://twitter.com/willpsng"><i class="fa fa-twitter"></i></a>
+    </p>
+</div>
