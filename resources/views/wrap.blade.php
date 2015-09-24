6da5765223ec6d71478e9d63326bb5a33cc7407e
William Naughton-Gravette <william@willng.me>
2015-09-24 15:06:27 +1000

    added animate.css

--- resources/views/wrap.blade.php
+++ resources/views/wrap.blade.php
@@ -13,6 +13,7 @@
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
+        <link rel="stylesheet" href="{{asset('css/animate.css')}}"/>
         <link rel="stylesheet" href="{{asset('css/rainbow-theme/github.css')}}"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="{{asset('js/highcharts.js')}}"/></script>
