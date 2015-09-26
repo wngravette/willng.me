cfd3a8ed0c7dedad1387806d9bdb7332ebf0034c
William Naughton-Gravette <william@willng.me>
2015-09-26 20:21:48 +1000

    spellcheck

--- resources/views/email/signup.blade.php
+++ resources/views/email/signup.blade.php
@@ -1,7 +1,7 @@
 @extends('email.wrap')
 @section('content')
 <p>Nice one,</p>
-<p>Now, whenever I make a new blog post, I'll harrass you via email about it.</p>
+<p>Now, whenever I make a new blog post, I'll harass you via email about it.</p>
 <p>Peace and blessings,</p>
 <p>William Naughton-Gravette.</p>
 @endsection
