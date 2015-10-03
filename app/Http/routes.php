06cd2ef06019d37bd44a5aaec93366a0e90da97a
William‌‌ Naughton-Gravette <william@willng.me>
2015-10-03 14:33:19 +1000

    re-add registration routes
    changed app key, password doesnt work #oops

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,9 +28,9 @@ Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-// Registration routes...
-//Route::get('auth/register', 'Auth\AuthController@getRegister');
-//Route::post('auth/register', 'Auth\AuthController@postRegister');
+Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 //Dev routes...
 Route::group(['middleware' => 'auth', 'prefix' => 'dev'], function () {
