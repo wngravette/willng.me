6db8bf7fc4dce53dd2d8b0d209e358d45db2ae74
William Naughton-Gravette <william@willng.me>
2015-10-03 16:05:03 +1000

    make blog post email clickable

--- resources/views/email/new-post.blade.php
+++ resources/views/email/new-post.blade.php
@@ -1,7 +1,7 @@
 @extends('email.wrap')
 @section('content')
 <p>I made a new blog post,</p>
-<h2>{{$article->article_headline}}</h2>
+<h2><a href="https://willng.me/{{$article->article_url}}">{{$article->article_headline}}</a></h2>
 <h3>{{$article->article_catch}}</h3>
 <br/>
 <p>Peace and blessings,</p>
