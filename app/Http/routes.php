f7d8c8c1b1415a1c422a4051c9a79a1075a5b399
William‌‌ Naughton-Gravette <william@willng.me>
2015-10-03 14:34:45 +1000

    unbreak

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,7 +28,7 @@ Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-Registration routes...
+// Registration routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 
